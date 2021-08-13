16686a5921c23e6dd98e21c11045352ad0a619cd
Killam-Git <zaidmallik9@gmail.com>
2021-08-13 10:11:05 +0530

    Initial commit

---
+++ Who's~At~The~Door.py
@@ -0,0 +1,62 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+
+path='Images'
+images = []
+Names = []
+mylist = os.listdir(path)
+
+for names in mylist:
+    name = cv2.imread(f'{path}/{names}')
+    images.append(name)
+    Names.append(os.path.splitext(names)[0])
+
+
+def findEncodings(listimages):
+    encodeList = []
+    for img in listimages:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+encodeListKnown = findEncodings(images)
+print('Encoding Complete')
+ 
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    sucess, img = cap.read()
+    imgS = cv2.resize(img, (0,0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+    faces_current_frame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, faces_current_frame)
+
+    for encodeFace, faceLoc in zip(encodesCurFrame, faces_current_frame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        facedis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        # print(facedis)
+        matchIndex = np.argmin(facedis)
+
+        if matches[matchIndex]:
+            who = Names[matchIndex].upper()
+            # print(who)
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+            cv2.rectangle(img, (x1,y1),(x2,y2), (0,0,255), 2)
+            cv2.rectangle(img, (x1, y2-35), (x2, y2), (0,0,255), cv2.FILLED)
+            cv2.putText(img, who, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2)
+        else:
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+            cv2.rectangle(img, (x1,y1),(x2,y2), (0,0,255), 2)
+            cv2.rectangle(img, (x1, y2-35), (x2, y2), (0,0,255), cv2.FILLED)
+            cv2.putText(img, "UNKNOWN", (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2)
+
+    cv2.imshow("webcam", img)
+    cv2.waitKey(1)
+    if cv2.waitKey(1) & 0xFF == ord('z'):
+        break
